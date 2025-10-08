856eac85fc77cbf10c30b6d81fda8508093ab6de
yngcmryl <yungcoriggy@gmail.com>
2025-10-08 22:55:08 +0800

    feature(app): register Product, Transaction, and TransactionItem models in admin

--- pos_app/admin.py
+++ pos_app/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Product, Transaction, TransactionItem
 
-# Register your models here.
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'stock', 'created_at', 'updated_at')
+    search_fields = ('name',)
+    list_filter = ('created_at', 'updated_at')
+
+
+@admin.register(Transaction)
+class TransactionAdmin(admin.ModelAdmin):
+    list_display = ('id', 'date', 'total', 'payment_method')
+    list_filter = ('payment_method', 'date')
+
+
+@admin.register(TransactionItem)
+class TransactionItemAdmin(admin.ModelAdmin):
+    list_display = ('transaction', 'product', 'quantity', 'subtotal')
+    list_filter = ('transaction',)
