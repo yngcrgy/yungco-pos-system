539d4a9af7e2382f6060b86224ffa1ee87bf4374
yngcmryl <yungcoriggy@gmail.com>
2025-10-08 23:31:28 +0800

    feature(app): add transaction history and sales report pages

--- pos_app/urls.py
+++ pos_app/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
     path('transaction/', views.process_transaction, name='process_transaction'),
     path('edit/<int:pk>/', views.edit_product, name='edit_product'),
     path('delete/<int:pk>/', views.delete_product, name='delete_product'),
+    path('transactions/', views.transaction_history, name='transaction_history'),
+    path('transactions/<int:pk>/', views.transaction_detail, name='transaction_detail'),
 
 ]
